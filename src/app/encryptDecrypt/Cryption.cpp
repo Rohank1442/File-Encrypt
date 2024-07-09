cb7b19d44574a6da51ca32564250ae24d16a7f09
Rohank1442 <rohankumar1442@gmail.com>
2024-07-10 01:08:21 +0530

    testing

--- src/app/encryptDecrypt/Cryption.cpp
+++ src/app/encryptDecrypt/Cryption.cpp
@@ -2,22 +2,28 @@
 #include "../processes/Task.hpp"
 #include "../fileHandling/ReadEnv.cpp"
 
-int executeCryption(const std::string& taskData) {
+int executeCryption(const std::string &taskData)
+{
     Task task = Task::fromString(taskData);
     ReadEnv env;
     std::string envKey = env.getenv();
     int key = std::stoi(envKey);
-    if (task.action == Action::ENCRYPT) {
+    if (task.action == Action::ENCRYPT)
+    {
         char ch;
-        while (task.f_stream.get(ch)) {
+        while (task.f_stream.get(ch))
+        {
             ch = (ch + key) % 256;
             task.f_stream.seekp(-1, std::ios::cur);
             task.f_stream.put(ch);
         }
         task.f_stream.close();
-    } else {
+    }
+    else
+    {
         char ch;
-        while (task.f_stream.get(ch)) {
+        while (task.f_stream.get(ch))
+        {
             ch = (ch - key + 256) % 256;
             task.f_stream.seekp(-1, std::ios::cur);
             task.f_stream.put(ch);
