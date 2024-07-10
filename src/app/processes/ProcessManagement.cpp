114c238aeb43d9ee1799e2acf720a7f55682bab6
Rohank1442 <rohankumar1442@gmail.com>
2024-07-10 13:08:34 +0530

    processmanagementforWindowsCompatibility

--- src/app/processes/ProcessManagement.cpp
+++ src/app/processes/ProcessManagement.cpp
@@ -1,8 +1,14 @@
 #include "ProcessManagement.hpp"
 #include <iostream>
 #include <cstring>
+#include "../encryptDecrypt/Cryption.hpp"
+
+#ifdef _WIN32
+#include <windows.h>
+#else
 #include <sys/wait.h>
-#include '../encryptDecrypt/Cryption.hpp'
+#endif
+
 
 ProcessManagement::ProcessManagement(){}
 
@@ -11,11 +17,53 @@ bool ProcessManagement::submitToQueue(std::unique_ptr<Task> task){
     return true;
 }
 
-void ProcessManagement::executeTasks(){
-    while(!taskQueue.empty()){
+void ProcessManagement::executeTasks() {
+    while(!taskQueue.empty()) {
         std::unique_ptr<Task> taskToExecute = std::move(taskQueue.front());
         taskQueue.pop();
-        std::cout<<"Executing task: "<<taskToExecute->toString()<<std::endl;
+        std::cout << "Executing task: " << taskToExecute->toString() << std::endl;
+        
+        #ifdef _WIN32
+        executeProcess(taskToExecute->toString());
+        #else
         executeCryption(taskToExecute->toString());
+        #endif
     }
 }
+
+#ifdef _WIN32
+bool ProcessManagement::executeProcess(const std::string& command) {
+    STARTUPINFOW si;
+    PROCESS_INFORMATION pi;
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    ZeroMemory(&pi, sizeof(pi));
+
+    std::wstring wideCommand = std::wstring(command.begin(), command.end());
+
+    bool success = CreateProcessW(
+        NULL,                   // No module name (use command line)
+        &wideCommand[0],        // Command line
+        NULL,                   // Process handle not inheritable
+        NULL,                   // Thread handle not inheritable
+        FALSE,                  // Set handle inheritance to FALSE
+        0,                      // No creation flags
+        NULL,                   // Use parent's environment block
+        NULL,                   // Use parent's starting directory 
+        &si,                    // Pointer to STARTUPINFO structure
+        &pi                     // Pointer to PROCESS_INFORMATION structure
+    );
+
+    if (!success) {
+        std::cerr << "CreateProcess failed. Error: " << GetLastError() << std::endl;
+        return false;
+    }
+
+    WaitForSingleObject(pi.hProcess, INFINITE);
+
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+
+    return true;
+}
+#endif
--- src/app/processes/ProcessManagement.hpp
+++ src/app/processes/ProcessManagement.hpp
@@ -1,19 +1,30 @@
 #ifndef PROCESS_MANAGEMENT_HPP
 #define PROCESS_MANAGEMENT_HPP
 
-#include "Task.hpp"
+#pragma once
 #include <queue>
 #include <memory>
+#include "Task.hpp"
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <sys/wait.h>
+#endif
 
 class ProcessManagement
 {
-    public:
-        ProcessManagement();
-        bool submitToQueue(std::unique_ptr<Task> task);
-        void executeTasks();
+public:
+    ProcessManagement();
+    bool submitToQueue(std::unique_ptr<Task> task);
+    void executeTasks();
 
-    private:
-        std::queue<std::unique_ptr<Task>> taskQueue;
+#ifdef _WIN32
+    bool executeProcess(const std::string &command);
+#endif
+
+private:
+    std::queue<std::unique_ptr<Task>> taskQueue;
 };
 
 #endif
