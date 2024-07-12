54de5bd7f5e83443959fe840d441d0a194094311
Rohank1442 <rohankumar1442@gmail.com>
2024-07-12 22:40:15 +0530

    check

--- src/app/processes/ProcessManagement.hpp
+++ src/app/processes/ProcessManagement.hpp
@@ -6,23 +6,13 @@
 #include <memory>
 #include "Task.hpp"
 
-#ifdef _WIN32
-#include <windows.h>
-#else
-#include <sys/wait.h>
-#endif
-
 class ProcessManagement
 {
 public:
     ProcessManagement();
     bool submitToQueue(std::unique_ptr<Task> task);
     void executeTasks();
-
-#ifdef _WIN32
-    bool executeProcess(const std::string &command);
-#endif
-
+    
 private:
     std::queue<std::unique_ptr<Task>> taskQueue;
 };
